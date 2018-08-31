77cc54d1b37823195cde1add3b6691d580ce4c12
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-08-31 16:26:01 -0500

    beginning stylistic changes and button functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,37 @@
+
 var DarkMagician = {
     battk: 5,
     cattk: 4,
     hp: 100,
     name: "Dark Magician",
-    bio: "A magician of immense power favored by a certain King of Games."
+    bio: "A magician of immense power favored by a certain King of Games.",
+    portrait: src="assets/images/dark-magician.png"
 }
 var Merlin = {
     battk: 6,
     cattk: 3,
     hp: 85,
     name: "Merlin",
-    bio: "A powerful wizard featured in legends."
+    bio: "A powerful wizard featured in legends.",
+    portrait: src="assets/images/merlin.jpg"
 }
 var Yen = {
     battk: 7,
     cattk: 6,
     hp: 75,
     name: "Yennefer of Vengerberg",
-    bio: "An unparalleled sorceress, her beauty only matched with her biting wit."
+    bio: "An unparalleled sorceress, her beauty only matched with her biting wit.",
+    portrait: src="assets/images/yen.jpg"
 }
 var Jareth = {
     battk: 4,
     cattk: 7,
     hp: 115,
     name: "Jareth the Goblin King",
-    bio: "A mysterious king with magical abilities, fond of toying with mortals." 
+    bio: "A mysterious king with magical abilities, fond of toying with mortals.",
+    portrait: src="assets/images/jareth2.jpg"
 }
+DarkMagician = document.getElementById("p1");
+Merlin = document.getElementById("p2");
+Yen = document.getElementById("p3");
+Jareth = document.getElementById("p4");
