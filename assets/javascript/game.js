ab183e9b2e1fe24d82e4d5516c660aba430e2167
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-03 20:32:25 -0500

    style changes, beginning work on game part

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,48 @@
 
 var DarkMagician = {
-    battk: 5,
-    cattk: 4,
+    battk: 15,
+    cattk: 14,
     hp: 100,
     name: "Dark Magician",
     bio: "A magician of immense power favored by a certain King of Games.",
     portrait: src = "assets/images/dark-magician.png"
 }
 var Merlin = {
-    battk: 6,
-    cattk: 3,
+    battk: 16,
+    cattk: 13,
     hp: 85,
     name: "Merlin",
     bio: "A powerful wizard featured in countless legends.",
     portrait: src = "assets/images/merlin.jpg"
 }
 var Yen = {
-    battk: 7,
-    cattk: 6,
+    battk: 17,
+    cattk: 16,
     hp: 75,
     name: "Yennefer of Vengerberg",
     bio: "An unparalleled sorceress, her beauty only matched with her biting wit.",
     portrait: src = "assets/images/yen.jpg"
 }
 var Jareth = {
-    battk: 4,
-    cattk: 7,
+    battk: 14,
+    cattk: 17,
     hp: 115,
     name: "Jareth the Goblin King",
     bio: "A mysterious king with magical abilities, fond of toying with mortals.",
     portrait: src = "assets/images/jareth2.jpg"
 }
-var player 
-var defender 
 
+
+var player 
+var defender
+
+var castSpell = function() {
+    defender.hp - player.attk;
+             console.log("player battk:" + player.battk)
+            document.getElementById("playerHP").innerHTML = "HP : " + player.hp;
+            document.getElementById("defenderHP").innerHTML = "HP : " + defender.hp;
+            
+             console.log("defender hp:" + defender.hp)
+console.log(defender.hp);
+
+}
